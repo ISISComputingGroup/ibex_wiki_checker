46bb57fcc09fe4a74f069d391bb836bacd649a3f
Adrian Potter <Adrian.Potter@tessella.com>
2018-02-07 10:46:16 +0000

    Add encoding to ignored words to allow for umlauts

--- tests/page_tests.py
+++ tests/page_tests.py
@@ -7,7 +7,7 @@ from enchant.tokenize import URLFilter, EmailFilter, WikiWordFilter, MentionFilt
 
 def get_ignored_words():
     # We're finding it easier to work with ignored words ourselves rather than using Enchant's in-built
-    with open("words.txt", "r") as f:
+    with open("words.txt", "r", encoding="utf-8") as f:
         words = f.read().split()
     return words
 
