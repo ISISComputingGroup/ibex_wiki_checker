bbd9096f3f551ac9194cbb159190e7336e0581f6
Liam <02liamp@gmail.com>
2018-11-06 09:46:10 +0000

    improved script

--- sort.sh
+++ sort.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-tr '[:upper:]' '[:lower:]' < words.txt > words-lower.txt 
-sort words-lower.txt > words-sorted.txt | mv words-sorted.txt words.txt
-rm words-lower.txt 
+sort words.txt | tr '[:upper:]' '[:lower:]' > sorted.txt
+mv sorted.txt words.txt
