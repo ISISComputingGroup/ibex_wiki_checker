9d0887a05770e6b433a8dd4702367c84e5ee7557
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2021-11-24 10:43:50 +0000

    Update run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -1,5 +1,7 @@
+setlocal
 set PYTHONIOENCODING=utf-8
 set PYTHONUNBUFFERED=TRUE
+del /s /q test-reports\*.xml
 "%~dp0Python\python.exe" -m pip install requests
 "%~dp0Python\python.exe" -m pip install mock
-"%~dp0Python\python.exe" run_tests.py --remote
+"%~dp0Python\python.exe" -u run_tests.py --remote
