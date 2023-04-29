4c6d115b6591f2d1857dcfd99972a86fde56c87d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-04-30 00:56:45 +0100

    Update run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -8,8 +8,9 @@ if exist "ibex_utils" rd /q /s ibex_utils
 git clone https://github.com/ISISComputingGroup/ibex_utils.git
 CALL ibex_utils\installation_and_upgrade\define_latest_genie_python.bat
 @echo on
-"%LATEST_PYTHON%" -m venv %~dp0venv
-CALL venv\Scripts\activate.bat
+if exist "my_venv" rd /q /s my_venv 
+"%LATEST_PYTHON%" -m venv %~dp0my_venv
+CALL my_venv\Scripts\activate.bat
 
 REM Clean local python environment and install requirements
 python -m pip freeze --local > toberemoved.txt 
@@ -18,6 +19,6 @@ python -m pip install -r requirements.txt
 
 REM make a python3.exe to avoid being terminated
 REM by stop_ibex_server
-copy venv\Scripts\python.exe venv\Scripts\python3.exe 
+copy my_venv\Scripts\python.exe my_venv\Scripts\python3.exe 
 REM run tests
 python3 -u run_tests.py --remote
