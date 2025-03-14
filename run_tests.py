ab757d725a7d8f9098c9bc5fc0a4f787871767d5
Jack Harper <harps912@gmail.com>
2025-03-14 10:02:00 +0000

    remove debug statement

--- run_tests.py
+++ run_tests.py
@@ -135,15 +135,6 @@ def run_all_tests(single_file, remote, folder):
 
 
 def main():
-
-    import enchant
-    print("TESTTTTTTTT")
-    broker = enchant.Broker()
-    broker.describe()
-    print(broker.list_languages())
-    return 0
-
-
     locale.setlocale(locale.LC_ALL, 'en_GB.UTF-8')
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
